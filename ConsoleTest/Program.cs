fea1de7c2c08e520fe93523822eabff08a844f3f
Olivier MATROT <omatrot@free.fr>
2020-11-20 09:58:51 +0100

    first commit

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LinqToEdmx;
+using LinqToEdmx.Map;
+using LinqToEdmx.Model.Conceptual;
+using LinqToEdmx.Model.Storage;
+
+Console.WriteLine("Hello World!");
+
+var edmx = Edmx.Load(@"/Users/omatrot/Downloads/CommonModel/CommonModel.edmx");
+
+var entityTypes = edmx.GetItems<EntityType>();
+
+var entityTypeStores = edmx.GetItems<EntityTypeStore>();
+
+var entityTypeMappings = edmx.GetItems<EntityTypeMapping>();
+
+Console.ReadKey();
