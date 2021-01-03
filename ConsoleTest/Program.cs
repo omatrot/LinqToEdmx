b4ab54a8e3d277ea7fb2ae76efd1039c1783b79e
Olivier MATROT <omatrot@free.fr>
2021-01-03 15:42:29 +0100

    Test the framework

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -12,24 +12,107 @@ Console.WriteLine("Hello World!");
 //    Console.WriteLine(entity.Name);
 //}
 
-var edmxV3 = LinqToEdmx.EdmxV3.Load(@"../../../AventureWorks2019.edmx");
+var edmxPath = @"../../../AventureWorks2019.edmx";
 
-var entityTypes = edmxV3.GetItems<LinqToEdmx.Model.ConceptualV3.EntityType>();
+// Assume EDMX version is 3.0
 
-foreach (var entity in entityTypes)
+var edmxv3 = LinqToEdmx.EdmxV3.Load(edmxPath);
+
+if (edmxv3.Version == "3.0")
 {
-    Console.WriteLine(entity.Name);
+    string GetSchemaName(string entityName)
+    {
+        var entitySetContainer = edmxv3.GetItems<LinqToEdmx.Model.StorageV3.EntityContainer>();
+        foreach(var container in entitySetContainer)
+        {
+            foreach (var entitySet in container.EntitySets.Where(es => es.Type == "Tables"))
+            {
+                if (entitySet.Name == entityName)
+                {
+                    return entitySet.Schema;
+                }
+            }
+            // FIXME Return the correct schema for views
+            //foreach (var entitySet in container.EntitySets.Where(es => es.Type == "Views"))
+            //{
+            //    if (entitySet.EntityType.Contains(entityName))
+            //    {
+            //        return entitySet.Schema;
+            //    }
+            //}
+        }
+        return string.Empty;
+    }
+
+    var entityTypes = edmxv3.GetItems<LinqToEdmx.Model.ConceptualV3.EntityType>();
+
+    foreach (var entity in entityTypes)
+    {
+        Console.WriteLine(string.Concat(GetSchemaName(entity.Name), ".", entity.Name));
+
+        // PK columns
+        foreach (var pref in entity.Key.PropertyRefs)
+        {
+            Console.WriteLine(@$" PK => [{pref.Name}]");
+        }
+
+        // Scalar properties
+        foreach (var column in entity.Properties)
+        {
+            Console.WriteLine(@$" P => [{column.Name}] IsNullable [{column.Nullable}] ");
+        }
+
+        // Navigation properties
+        foreach (var column in entity.NavigationProperties)
+        {
+            Console.WriteLine(@$" P => [{column.Name}]");
+        }
+
+        //foreach(var fk in entity.)
+    }
+
+    var entityTypeStores = edmxv3.GetItems<LinqToEdmx.Model.StorageV3.EntityTypeStore>();
+
+    foreach (var entityTypeStore in entityTypeStores)
+    {
+        Console.WriteLine(entityTypeStore.Name);
+    }
+
+    var entityTypeMappings = edmxv3.GetItems<LinqToEdmx.MapV3.EntityTypeMapping>();
+
+    foreach (var table in entityTypeMappings)
+    {
+        foreach (var fragment in table.MappingFragments)
+        {
+            Console.WriteLine(fragment.StoreEntitySet);
+        }
+    }
+
+    var entitySetContainer = edmxv3.GetItems<LinqToEdmx.Model.StorageV3.EntityContainer>();
+
+    Console.WriteLine(entityTypeMappings.Count());
 }
-
-var entityTypeStores = edmxV3.GetItems<LinqToEdmx.Model.StorageV3.EntityTypeStore>();
-
-foreach(var entityTypeStore in entityTypeStores)
+else if (edmxv3.Version == "2.0")
 {
-    Console.WriteLine(entityTypeStore.Name);
+    var edmxv2 = LinqToEdmx.EdmxV2.Load(edmxPath);
+
+
+    var entityTypes = edmxv2.GetItems<LinqToEdmx.Model.ConceptualV2.EntityType>();
+
+    foreach (var entity in entityTypes)
+    {
+        Console.WriteLine(entity.Name);
+    }
+
+    var entityTypeStores = edmxv2.GetItems<LinqToEdmx.Model.StorageV2.EntityTypeStore>();
+
+    foreach (var entityTypeStore in entityTypeStores)
+    {
+        Console.WriteLine(entityTypeStore.Name);
+    }
+
+    var entityTypeMappings = edmxv2.GetItems<LinqToEdmx.MapV2.EntityTypeMapping>();
+
+    Console.WriteLine(entityTypeMappings.Count());
 }
-
-var entityTypeMappings = edmxV3.GetItems<LinqToEdmx.MapV3.EntityTypeMapping>();
-
-Console.WriteLine(entityTypeMappings.Count());
-
 Console.ReadKey();
